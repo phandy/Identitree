a515b2dc13ba0282515a7d5bc05f3b617f0932a9
Andy Phan <phandy@gmail.com>
2013-10-21 21:04:30 -0400

    New Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Identitree::Application.config.secret_token = '55e8c674ce926dc527115febd24ee3173409f6e1e00f178f473f072a7f3c9c8a570286dcaae919aac62f9d0f04a4f9c360ea6902134e4e3494e0ed61eb49f4c3'
